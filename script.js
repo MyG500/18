dd81b8c5d97d4517f5b3cdcb9af456ebf6d62dd7
MyG500 <111442725+MyG500@users.noreply.github.com>
2022-11-02 16:31:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+class user1 {
+    constructor(Fname, Lname, home) {
+        this.Lname = Lname;
+        this.Fname = Fname;
+        this.home = home;
+        this.books = [];
+        book(){
+            push(this.books);
+
+        }
+    }
+
+}
